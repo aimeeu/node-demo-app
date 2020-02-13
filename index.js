d342f41f64c44a6a0cf2eeed4edbf06616c68f4a
aimeeu <aimeeu.opensource@gmail.com>
2020-02-13 16:25:55 -0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World!');
+  res.send('WE ARE THE BORG. RESISTENCE IS FUTILE!');
 });
 
 var server = app.listen(3000, function () {
